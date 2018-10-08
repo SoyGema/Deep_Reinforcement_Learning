4ae8e8cb99ab7152bd3687f9fe6e979ab1956ee0
Gema Parreño <gema.parreno.piqueras@gmail.com>
2018-10-08 11:15:14 +0200

    Create Makefile.sh

---
+++ Makefile.sh
@@ -0,0 +1,23 @@
+.PHONY: test
+
+## CONFIG
+
+config:
+  sudo apt-get install python3-pip
+  pip3 install keras
+  pip3 install keras-rl
+  pip3 install tensorflow
+  pip3 install gym
+  pip3 install matplotlib
+  sudo apt-get install htop
+
+## DOWNLOAD REPOSITORY
+
+
+## LAUNCH DQN
+
+
+## LAUNCH DQN-DOUBLE
+
+
+## LAUNCH DQN-DUELING
